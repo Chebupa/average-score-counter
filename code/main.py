ae91c64132aa45ee7733089e5c483ada01548390
Chebupela <aristarhsakulin@gmail.com>
2023-02-18 09:42:55 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -12,13 +12,38 @@ while True:
         print('Something went wrong')
 
 # collecting info
-fiveAmount = int(input('How many "5s" do you have?\n'))
-fourAmount = int(input('How many "4s" do you have?\n'))
-threeAmount = int(input('How many "3s" do you have?\n'))
-twoAmount = int(input('How many "2s" do you have?\n'))
+while True:
+    fiveAmount = int(input('How many "5s" do you have?\n'))
+    if type(enter) != int:
+        print('Something went wrong')
+    else:
+        break
+while True:
+    fourAmount = int(input('How many "4s" do you have?\n'))
+    if enter == "":
+        break
+    else:
+        print('Something went wrong')
+while True:
+    threeAmount = int(input('How many "3s" do you have?\n'))
+    if enter == "":
+        break
+    else:
+        print('Something went wrong')
+while True:
+    twoAmount = int(input('How many "2s" do you have?\n'))
+    if enter == "":
+        break
+    else:
+        print('Something went wrong')
 
 # algorythm
 def averageFn(five, four, three, two):
+    five = five * 5
+    four = four * 4
+    three = three * 3
+    two = two * 2
+
     result = (five + four + three + two)/2
     return result
 
@@ -29,7 +54,7 @@ print(f'Your average is: {result}')
 
 # exit
 while True:
-    enter = input('Press "Enter" to begin...')
+    enter = input('Press "Enter" to exit.')
     if enter == "":
         break
     else:
